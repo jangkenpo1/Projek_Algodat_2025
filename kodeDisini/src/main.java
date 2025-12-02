b236661e4becfafc7c73d36b334dd36515f64e45
indarrputri15 <indarrputri@gmail.com>
2025-12-02 23:32:36 +0700

    Menambahkan node dan edge pada main.java

--- kodeDisini/src/main.java
+++ kodeDisini/src/main.java
@@ -4,8 +4,14 @@ public class main {
 
     graph.addNode("A1", "Gedung");
     graph.addNode("B2", "SPBU");
+    graph.addNode("C3", "Rumah");
+    graph.addNode("D4", "Pabrik");
+    graph.addNode("E5", "Apartemen");
 
     graph.doubleaddEdge("A1", "B2", 10);
-
+    graph.doubleaddEdge("A1", "C3", 5);
+    graph.doubleaddEdge("B2", "D4", 15);
+    graph.doubleaddEdge("C3", "E5", 7);
+    graph.doubleaddEdge("D4", "E5", 12);
     }
 }
